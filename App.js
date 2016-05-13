c179fc7f2e45e35075ec948d3740c6f64e423171
Sebastian Nilsson <sebbehebbe@gmail.com>
2016-05-13 07:59:39 +0200

    Moar files

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      txt: 'This is the state text',
+      cat: 0
+    }
+  }
+  update(e) {
+    this.setState({txt: e.target.value})
+  }
+  render() {
+    return (
+      <div>
+        <input type="text" onChange={this.update.bind(this)}/>
+        <h1>{this.state.txt}</h1>
+      </div>
+  );
+  }
+}
+
+export default App;
